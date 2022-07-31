47c3ad50052c5931b3dfdb052b975591f8e33b4a
1042545965@qq.com <dkz13887548339>
2022-07-31 23:09:18 +0800

    整合完毕

--- cloud-provider-payment8001/src/main/java/com/dkz/springcloud/payment/PaymentApplication.java
+++ cloud-provider-payment8001/src/main/java/com/dkz/springcloud/payment/PaymentApplication.java
@@ -3,8 +3,10 @@ package com.dkz.springcloud.payment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan("com.dkz.springcloud.payment.mapper")
 public class PaymentApplication {
 
