4c6846bdc393fc20bcae16c316d6919d360bcf58
1042545965@qq.com <dkz13887548339>
2022-08-03 23:24:26 +0800

    Hystrix Dashboard

--- cloud-provider-payment8001/src/main/java/com/dkz/springcloud/payment/PaymentApplication.java
+++ cloud-provider-payment8001/src/main/java/com/dkz/springcloud/payment/PaymentApplication.java
@@ -1,19 +1,37 @@
 package com.dkz.springcloud.payment;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableCircuitBreaker
 @MapperScan("com.dkz.springcloud.payment.mapper")
 public class PaymentApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(PaymentApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+
+    @Bean
+    public ServletRegistrationBean getservlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsstreamservlet");
+        return registrationBean;
+
+    }
+
 
 }
